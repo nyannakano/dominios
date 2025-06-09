955c76ef96ef254d89755c0f702f02dc9fd00a5f
Gabriel Nakano <gabrielnakano123@gmail.com>
2025-06-09 00:03:58 -0300

    Middleware on back-end

--- dominios-back/routes/api.php
+++ dominios-back/routes/api.php
@@ -9,11 +9,13 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/domains', [DomainController::class, 'getDomains']);
-Route::get('/domain/{id}', [DomainController::class, 'getDomain']);
-Route::post('/domain-create', [DomainController::class, 'createDomain']);
-Route::delete('/domain-delete/{id}', [DomainController::class, 'deleteDomain']);
-Route::put('/domain-update/{id}', [DomainController::class, 'updateDomain']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/domains', [DomainController::class, 'getDomains']);
+    Route::get('/domain/{id}', [DomainController::class, 'getDomain']);
+    Route::post('/domain-create', [DomainController::class, 'createDomain']);
+    Route::delete('/domain-delete/{id}', [DomainController::class, 'deleteDomain']);
+    Route::put('/domain-update/{id}', [DomainController::class, 'updateDomain']);
+});
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
